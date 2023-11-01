06467e04d02c1c6a8e7dbcb91266d8a9420b5831
reddivarinandana <nadanareddy1122@gmail.com>
2023-11-02 04:19:16 +0530

    SubTopic Data

---
+++ Server-Side/shecodes-programming/src/main/java/com/shecodesprogramming/shecodesprogramming/model/SubTopic.java
@@ -0,0 +1,4 @@
+package com.shecodesprogramming.shecodesprogramming.model;
+
+public class SubTopic {
+}
---
+++ Server-Side/shecodes-programming/src/main/java/com/shecodesprogramming/shecodesprogramming/repositories/SubTopicRepository.java
@@ -0,0 +1,4 @@
+package com.shecodesprogramming.shecodesprogramming.repositories;
+
+public interface SubTopicRepository {
+}
